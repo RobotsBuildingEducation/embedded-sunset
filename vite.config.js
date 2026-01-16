e031176c6102ab6de3f319e4dc5943c2e57bcf10
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2026-01-16 16:26:52 -0600

    store

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     react(),
     VitePWA({
       workbox: {
-        maximumFileSizeToCacheInBytes: 7000000, // Set to 4MB or any higher value
+        maximumFileSizeToCacheInBytes: 7100000, // Set to 4MB or any higher value
       },
       manifest: {
         name: "Robots Building Education",
